07d18643be8e75e01e92999d75db89a1a330fd91
xscript <xscript>
2004-06-11 15:43:12 +0000

    Ja funciona el nostre sistema de fitxers!!!!!!!!!
    ole!!!!

--- codi/mosix.c
+++ codi/mosix.c
@@ -1,10 +1,11 @@
+
 #include "generic.h"
 #include <linux/mm.h>
 
 
 //struct cluster_t clus = {.nom = "123",
 //			 .next = NULL};
-
+/*
 struct node_t n3 = {	.nom = "n3",
 			.next = NULL};
 struct node_t n2 = {	.nom = "n2",
@@ -20,6 +21,12 @@ struct proces_t p2 = {	.nom = "proc2",
 struct proces_t p1 = {	.nom = "proc1",
 			.next = &p2};
 
+*/			
+int num_clusters() 
+{
+	return 1;
+}
+
 int llistar_clusters (struct cluster_t *llista)
 {
 	//llista = (struct cluster_t *)kmalloc(sizeof(struct cluster_t) ,GFP_KERNEL);	
@@ -28,19 +35,38 @@ int llistar_clusters (struct cluster_t *llista)
 	return 0;
 }
 
-int llistar_nodes (struct cluster_t *cluster_id, struct node_t *llista)
+int num_nodes (struct cluster_t *cluster_id)
 {
-	//llista[0] = &n1;
-	//llista[1] = &n2;
-	//llista[2] = &n3;
-	
+	return 3;
 }
 
-int liistar_procs (struct node_t *node_id, struct proces_t *llista)
+int llistar_nodes (struct cluster_t *cluster_id, struct node_t *llista)
 {
-	//llista[0] = &p1;
-	//llista[1] = &p2;
-	//llista[2] = &p3;
+	llista[0].nom = "n0";
+	llista[0].next = &llista[1];
+	llista[1].nom = "n1";
+	llista[1].next = &llista[2];
+	llista[2].nom = "n2";
+	llista[2].next = NULL;
+
+	return 0;
+}
+
+int num_procs (struct cluster_t *cluster_id, struct node_t *node_id) 
+{
+	return 3;
+}
+
+int llistar_procs (struct cluster_t *cluster_id, struct node_t *node_id, struct proces_t *llista)
+{
+	llista[0].nom = "p0";
+	llista[0].next = &llista[1];
+	llista[1].nom = "p1";
+	llista[1].next = &llista[2];
+	llista[2].nom = "p2";
+	llista[2].next = NULL;
+
+	return 0;
 }
 
 
