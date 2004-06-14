3e38bb0a196c6a61ad7567264578263429266dac
xscript <xscript>
2004-06-14 15:54:51 +0000

    Hasta aqui hemos llegado, acceder a algo que se genera dinamicamente es un poco dificil :-S. Lluis: El directori "code" no compila, per aixo hem seguit fent servir el directori "codi"...ho sento.

--- codi/mosix.c
+++ codi/mosix.c
@@ -23,8 +23,10 @@ int llistar_clusters (struct cluster_t *llista)
 int num_nodes (struct cluster_t *cluster_id)
 {
 	
-	struct proc_dir_entry *aux = proc_root.subdir;
+	struct proc_dir_entry *aux;
 	int res = 0;
+
+	printk (" **mosix.c: num_nodes\n");
 	
 	// Busquem el proc_dir_entry de /proc/hpc
 	aux = cerca_proc(&proc_root,"hpc");
@@ -39,14 +41,18 @@ int num_nodes (struct cluster_t *cluster_id)
 		res++;
 		aux = aux->next;
 	}
-
+	
+	printk("  ** mosix.c:  El numero de nodes es: %d\n",res);
 	return res;
 }
 
 int llistar_nodes (struct cluster_t *cluster_id, struct node_t *llista)
 {
-	struct proc_dir_entry *aux = proc_root.subdir;
+
+	struct proc_dir_entry *aux;
 	int i;
+	
+	printk (" ** mosix.c: llistar_nodes\n");
 	
 	// Busquem el proc_dir_entry de /proc/hpc
 	aux = cerca_proc(&proc_root,"hpc");
@@ -54,10 +60,11 @@ int llistar_nodes (struct cluster_t *cluster_id, struct node_t *llista)
 	// Entrem al directori nodes
 	aux = cerca_proc(aux,"nodes");
 	
-	// Assignem els nomos dels nodes que te
+	// Assignem els noms dels nodes que te
 	aux = aux->subdir;
 	i=0;
 	while (aux != NULL ) {		
+		printk("  ** mosix.c: llistar_nodes -> generando nodo:%s\n", aux->name);
 		llista[i].nom=aux->name;
 		if (aux->next==NULL)
 			llista[i].next=NULL;
@@ -109,9 +116,12 @@ struct proc_dir_entry* cerca_proc(struct proc_dir_entry *parent, char *nom) {
 	struct proc_dir_entry *aux = parent->subdir; 
 	
 	while (aux != NULL && trobat==0) {
+		printk("  ** mosix.c: cerca_proc Estoy en:%s\n",aux->name);
 		if (!strcmp(aux->name,nom)) trobat = 1;
 		else aux = aux->next;
 	}
 	
+	if (aux==NULL) printk("  ** mosix.c : cerca_proc -> Ha pasado algo mu malo!\n");
+	else printk("  ** mosix.c : cerca_proc -> directorio buscado:%s, encontrado:%s\n",nom,aux->name);
 	return aux;
 }
